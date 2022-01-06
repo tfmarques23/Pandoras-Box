ff51152b63b6096f548e99550c507371a1cf6e6d
tfmarques23 <92591638+tfmarques23@users.noreply.github.com>
2022-01-06 20:38:25 +0000

    Delete c directory

--- c/buff_oflw/findgrootadd.c
+++
@@ -1,9 +0,0 @@
-/*Get GROOT variable's address. Use this address to insert into shellcode */
-
-#include <unistd.h>
-
-int main(void)
-{
-    printf("GROOT address: 0x%lx\n", getenv("GROOT"));
-    return 0;
-}
